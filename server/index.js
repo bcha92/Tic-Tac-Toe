381812b1e006c3a9c6617a6d2a56a117a2077757
bcha92@hotmail.com <bcha92@hotmail.com>
2021-10-23 13:47:35 -0400

    --initialize node express server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,20 @@
-let msg = "hello world";
-console.log(msg);
+// Express Modules
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+// Local PORT to host application || 4000 by default
+const PORT = process.env.PORT || 4000;
+const app = express(); // Express App
+app.use(cors()) // Cross-Origin Resource Sharing
+app.use(morgan("tiny")); // Logger Middleware for http request errors
+
+// Endpoints
+
+
+// Error Handling
+app.get("*", (req, res) => res.status(404).json(
+    "Error 404: This is an error. Please check your endpoints."
+));
+// Ready to listen on this port...
+app.listen(PORT, () => console.log(`Standing by on PORT ${PORT}...`));
