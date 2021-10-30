dde93a5fe351e6b980ea81b0cb314918a6265872
bcha92@hotmail.com <bcha92@hotmail.com>
2021-10-30 15:42:45 -0400

    --adjusted global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import styled from "styled-components";
 // GlobalStyles CSS via styled-components
 import GlobalStyles from "./GlobalStyles";
 
 // Main App Component
 const App = () => {
-  return <>
+  return <AppWrapper>
     <GlobalStyles />
-    <h1>HELLO TIC TAC TOE</h1>
-  </>
+    <BrowserRouter>
+    <Header>
+      <h1>TIC TAC TOE</h1>
+    </Header>
+
+    <Switch>
+      <Route exact path="/">Hello</Route>
+      <Route path="/test">Test</Route>
+    </Switch>
+    </BrowserRouter>
+  </AppWrapper>
 }
+
+// Styled Components
+const AppWrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+`;
+
+const Header = styled.header`
+  top: 0;
+  padding: 10px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+`;
 
 export default App;
